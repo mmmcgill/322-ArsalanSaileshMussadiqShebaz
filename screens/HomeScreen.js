796f9f415ed56e23820e7b40d908e2580988968c
Arsalan Bin Najeeb <abnajeeb@knox.edu>
2020-04-21 13:56:53 -0500

    Main Screen with Bottom Nav bar
    Added main screen with Play, option and Credits button. Bottom Navbar has home button and link button

---
+++ screens/HomeScreen.js
@@ -0,0 +1,144 @@
+import * as WebBrowser from 'expo-web-browser';
+import * as React from 'react';
+import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+
+import { MonoText } from '../components/StyledText';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      
+      <View style={styles.welcomeContainer}>
+        <Text style={{fontSize: 40}}>
+          SCRUB
+        </Text>
+      </View>
+
+        <View style={{paddingBottom: 40}}>
+        <TouchableOpacity>
+          <View style={styles.buttoncont}>
+            <Text style={styles.buttontext}>Play</Text>
+          </View>
+        </TouchableOpacity>
+        </View>
+
+        <View style={{paddingBottom: 40}}>
+        <TouchableOpacity>
+          <View style={styles.buttoncont}>           
+           <Text style={styles.buttontext}>Options</Text>
+          </View>
+        </TouchableOpacity>
+        </View>
+
+        <TouchableOpacity>
+          <View style={styles.buttoncont}>           
+           <Text style={styles.buttontext}>Credits</Text>
+          </View>
+        </TouchableOpacity>
+        
+
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: 100,
+  },
+  developmentModeText: {
+    marginBottom: 20,
+    color: 'rgba(0,0,0,0.4)',
+    fontSize: 14,
+    lineHeight: 19,
+    textAlign: 'center',
+  },
+  buttoncont: {
+    alignItems: 'center',
+    padding: 12,
+    backgroundColor: 'lightblue',
+    borderRadius: 50,
+  },
+  buttontext: {
+    margin: 10,
+  },
+  contentContainer: {
+    paddingTop: 30,
+  },
+  welcomeContainer: {
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 80,
+  },
+  welcomeImage: {
+    width: 100,
+    height: 80,
+    resizeMode: 'contain',
+    marginTop: 3,
+    marginLeft: -10,
+  },
+  getStartedContainer: {
+    alignItems: 'center',
+    marginHorizontal: 50,
+  },
+  homeScreenFilename: {
+    marginVertical: 7,
+  },
+  codeHighlightText: {
+    color: 'rgba(96,100,109, 0.8)',
+  },
+  codeHighlightContainer: {
+    backgroundColor: 'rgba(0,0,0,0.05)',
+    borderRadius: 3,
+    paddingHorizontal: 4,
+  },
+  getStartedText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 24,
+    textAlign: 'center',
+  },
+  tabBarInfoContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: -3 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+      },
+      android: {
+        elevation: 20,
+      },
+    }),
+    alignItems: 'center',
+    backgroundColor: '#fbfbfb',
+    paddingVertical: 20,
+  },
+  tabBarInfoText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    textAlign: 'center',
+  },
+  navigationFilename: {
+    marginTop: 5,
+  },
+  helpContainer: {
+    marginTop: 15,
+    alignItems: 'center',
+  },
+  helpLink: {
+    paddingVertical: 15,
+  },
+  helpLinkText: {
+    fontSize: 14,
+    color: '#2e78b7',
+  },
+});
